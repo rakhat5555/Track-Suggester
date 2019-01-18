3296705bc63aa6db5742bab4de29343ca47b44cf
rakhat5555 <rakhat5555@gmail.com>
2019-01-18 14:41:40 -0800

    add js branch

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#track-form").submit(function(event) {
+    event.preventDefault();
+    var nameInput = $("input#name").val();
+    var reasonInput = $("select#reason").val();
+    var areaInput = $("select#area").val();
+    var reasonInput = $("select#howfast").val();
+    var howfastInput = $("select#frontorback").val();
+    var frontBackInput = $("select#reaction").val();
+console.log("it is working")
+if (frontBackInput === "1") {
+  $("#java").show();
+  }
+  else if (reasonInput === "2"){
+    $("#php").show();
+  }
+  else {
+    $("#css").show();
+  }
+ });
+});
