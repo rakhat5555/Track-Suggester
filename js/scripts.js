7a1fa4fc0f2a496171c73e10e702d7b4edf5912c
rakhat5555 <rakhat5555@gmail.com>
2019-02-06 12:01:05 -0800

    add show and hide , add button to change background color

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,29 @@ $(document).ready(function() {
     var nameInput = $("input#name").val();
     var reasonInput = $("select#reason").val();
     var areaInput = $("select#area").val();
-    var reasonInput = $("select#howfast").val();
-    var howfastInput = $("select#frontorback").val();
-    var frontBackInput = $("select#reaction").val();
+    var howfastInput = $("select#howfast").val();
+    var frontBackInput = $("select#frontorback").val();
+    var reactionInput = $("select#reaction").val();
 console.log("it is working")
 if (frontBackInput === "1") {
   $("#java").show();
   }
-  else if (reasonInput ==="3"){
+  else if (reasonInput ==="2"){
     $("#php").show();
   }
   else {
     $("#css").show();
-  }
- });
+  };
+  });
+  $("button#grey").click(function(){
+    $("body").addClass("grey-background");
+  });
+  $(".btn-test").click(function(){
+    $(".show-test").show();
+    $(".show-result").hide();
+  });
+  $(".btn-result").click(function(){
+    $(".show-result").show();
+    $(".show-test").hide();
+  });
 });
